98930c31b41f4e596110376d4c4b0893ab5faab3
thoamrigabito <tymagbito26@gmail.com>
2024-02-07 21:01:48 +0800

    new file:   public/assets/familyinternet.jpg 	modified:   resources/views/homepage.blade.php 	modified:   resources/views/include/applynow.blade.php 	modified:   resources/views/layout.blade.php

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -3,7 +3,7 @@
 
 @section('content')
 
-{{-- SIR Thom... Dito ka lagay nung pang landing page sir.
+    {{-- SIR Thom... Dito ka lagay nung pang landing page sir.
 wala na header ha? na include ko na kasi.
 no header and footer pls. Thanks.. --}}
 
@@ -13,48 +13,44 @@ no header and footer pls. Thanks.. --}}
 
     <div class="banner">
         <section class="chat">
-            <div class="head">
-                <h1>Chat with Us !</h1>
-                <p>Use our AJC Pisonet FB Messenger for quick and easy assistance.</p>
-                <div class="bannerbtn">
-                    <button><i class="fa-solid fa-comments btnicon"></i>UJC Chatbot</button>
-                </div>
+            <h1>Chat with Us !</h1>
+            <p>Use our AJC Pisonet FB Messenger for quick and easy assistance.</p>
+            <div class="bannerbtn">
+                <button><i class="fa-solid fa-comments btnicon"></i>UJC Chatbot</button>
             </div>
         </section>
-
         <section class="support">
-            <div class="head">
-                <h1>Support</h1>
-                <p>We’ll find the best solution for you.</p>
-                <div class="bannerbtn">
-                    <button><i class="fa-solid fa-circle-question btnicon"></i>Go to Support</button>
-                </div>
+            <h1>Support</h1>
+            <p>We’ll find the best solution for you.</p>
+            <div class="bannerbtn">
+                <button><i class="fa-solid fa-circle-question btnicon"></i>Go to Support</button>
             </div>
         </section>
     </div>
 
     <div>
         <section class="title_info">
-            <h1>We’ve got you covered with reliable internet that’s<br/> right for your home.</h1>
+            <h1>We’ve got you covered with reliable internet that’s right for your home.</h1>
         </section>
         <section class="info">
-            <div class="infos">
+            <div class="info1">
                 <img src="./assets/productivity.png">
                 <h4>Be more productive than ever</h4>
                 <p>with faster speeds that Fiber Unli can provide.</p>
             </div>
-            <div class="infos">
+            <div class="info2">
                 <img src="./assets/stream.png">
                 <h4>There’s no limit!</h4>
                 <p>Stream to your heart’s content for as low as P899!</p>
             </div>
-            <div class="infos">
+            <div class="info3">
                 <img src="./assets/internet.png">
-                <h4>Win more rounds with AUJ Internet</h4>
+                <h4>Win more rounds</h4>
+                <p>with AUJ Internet</p>
             </div>
         </section>
     </div>
-
+    <hr />
     <div class="plans_container">
         <section class="promos">
             <div class="promo_title">
@@ -65,14 +61,17 @@ no header and footer pls. Thanks.. --}}
                     <h3 class="mbps">200 Mbps</h3>
                     <p>Fiber Unli Plan 1699</p>
                 </div>
+
                 <div class="plan2">
                     <h3 class="mbps">400 Mbps</h3>
                     <p>Fiber Unli Plan 1699</p>
                 </div>
+
                 <div class="plan3">
                     <h3 class="mbps">600 Mbps</h3>
                     <p>Fiber Unli Plan 1699</p>
                 </div>
+
                 <div class="plan4">
                     <h3 class="mbps">1000 Mbps</h3>
                     <p>Fiber Unli Plan 1699</p>
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1,197 +1,292 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('title', 'AJC')</title>
-<style>
-    .nav-link.active{
-        color: red !important;
-    }
-    .login.active{
-        color: red !important;
-    }
-    .nav-link{
-        font-size: 20px;
-        text-align: center;
-    }
-    .nav-link:hover{
-        color: white;
-        text-decoration: underline;
-    }
-    .login{
-        text-decoration: none;
-        font-size: 20px;
-        color: rgb(209, 209, 209);
-    }
-    .login:hover{
-        text-decoration: underline;
-        color: white;
-    }
-    /* .form-control{
+    <style>
+        .nav-link.active {
+            color: red !important;
+        }
+
+        .login.active {
+            color: red !important;
+        }
+
+        .nav-link {
+            font-size: 20px;
+            text-align: center;
+        }
+
+        .nav-link:hover {
+            color: white;
+            text-decoration: underline;
+        }
+
+        .login {
+            text-decoration: none;
+            font-size: 20px;
+            color: rgb(209, 209, 209);
+        }
+
+        .login:hover {
+            text-decoration: underline;
+            color: white;
+        }
+
+        /* .form-control{
         border: none;
     } */
 
-    .banner{
-    display: flex;
-    align-items: center;
-    width: 100%;
-    }
 
-    .title{
-        padding: 40px 0;
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        justify-content: center;
-        background-color: rgba(162, 162, 162, 0.134);
-    }
+        /* Start homepage.blade CSS style */
+        h1,
+        h2,
+        h3.h4,
+        p {
+            background: transparent;
+        }
 
-    .title h1{
-        font-size: 38px;
-        color:#c43a3c;
-        background-color: transparent;
-    }
-    .chat{
-        width: 50%;
-        background-color: #F8EFE6;
-    }
-    .head {
-        padding: 20px 0;
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        justify-content: center;
-        background-color: transparent;
-    }
+        .title {
+            padding: 40px 0;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+            background-color: rgba(162, 162, 162, 0.134);
+        }
 
-    .head h1{
-        background: transparent;
-        font-size: 34px;
-        font-weight: 900;
-        padding: 50px 0 35px;
-    }
-    .head p{
-        background: transparent;
-    }
+        .title h1 {
+            font-size: 38px;
+            color: #c43a3c;
+            background-color: transparent;
+        }
 
-    .support{
-        align-items: center;
-        width:50%;
-        background-color:#F3EED8
-    }
+        .banner {
+            display: flex;
+            align-items: center;
+            width: 100%;
+        }
 
-    .bannerbtn{
-        background: transparent;
-    }
+        .chat {
+            width: 50%;
+            align-items: center;
+            background-color: #F8EFE6;
+            padding: 20px 0;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+            text-align: center;
+        }
 
-    .bannerbtn button{
-        margin:18px 0 0;
-        padding: .50em .30em;
-        font-size: 16px;
-        border-radius: 25px;
-        width: 178px;
-        border: none;
-        outline: none;
-        background-color: #c43a3c;
-        color:#fff;
-        font-size: 17px;
-        text-transform: uppercase;
-        justify-items: center;
-    }
+        .support {
+            align-items: center;
+            width: 50%;
+            background-color: #F3EED8;
+            padding: 20px 0;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+            text-align: center;
+        }
 
-    .btnicon{
-    background: transparent;
-    padding: 0 .5em 0 0;
-    }
+        .bannerbtn {
+            background: transparent;
+        }
 
-    .title_info{
-        padding-top: 50px;
-        font-size: 25px;
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        justify-content: center;
-        text-align: center;
-    }
+        .bannerbtn button {
+            margin: 18px 0 0;
+            padding: .50em .30em;
+            font-size: 16px;
+            border-radius: 25px;
+            width: 178px;
+            border: none;
+            outline: none;
+            background-color: #c43a3c;
+            color: #fff;
+            font-size: 17px;
+            text-transform: uppercase;
+            justify-items: center;
+        }
 
-    .info{
-        display: flex;
-        width: 100%;
-        justify-content: space-around;
-    }
+        .btnicon {
+            background: transparent;
+            padding: 0 .5em 0 0;
+        }
 
-    .infos{
-        padding: 20px 0;
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        justify-content: center;
-        background-color: transparent;
-    }
-    .infos img{
-        padding: 50px;
-    }
-    .infos p{
-        font-weight: lighter;
-    }
+        @media only screen and (max-width: 768px) {
+            .banner {
+                display: block
+            }
+
+            .chat,
+            .support {
+                width: 100%
+            }
+        }
 
 
-    .plans_container{
-        display: flex;
-        width: 100%;
-    }
-    .promos{
-        display: flex;
-        flex-direction: column;
-        width: 50%;
-        border: 1px solid black;
-        justify-content: center;
-        align-items: center;
-    }
-    .promo_title{
-        padding: 10px 0;
-    }
-    .plans{
-        display: grid;
-        grid-template-columns: repeat(2, 1fr);
-        grid-template-rows: repeat(2, 1fr);
-        grid-column-gap: 45px;
-        grid-row-gap: 10px;
-        padding: 45px;
+        .title_info {
+            padding: 35px 0;
+            font-size: 25px;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+            text-align: center;
+            text-wrap: wrap;
+            width:1000px;
+            margin: auto
+        }
 
-    }
+        .info {
+            display: flex;
+            width: 100%;
+            justify-content: space-around;
+            background: #d2cac24c
+        }
 
-    .plan1, .plan2, .plan3, .plan4{
-    padding: 20px;
-    width: 200px;
-    background-color: rgba(205, 205, 205, 0.214);
-    border: .10em solid rgba(122, 122, 122, 0.426);
-    border-radius: 20px;
-    }
+        .info1,.info2,.info3 {
+            padding: 20px 0;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+            background-color: transparent;
 
-    .mbps{
-        font-size: 30px;
-    }
+        }
 
-    .promo_details{
-    background-image: linear-gradient(rgba(255, 255, 255, 0.5), rgba(22, 21, 25, 0.7)), url(assets/familyinternet.jpg);
-    background-position: center;
-    background-size: cover;
-    background-repeat: no-repeat;
-    position: relative;
-    width: 50%;
-    padding: 20px;
-    }
+        .info1>img, .info2>img, .info3>img{
+            padding: 30px;
+        }
+        .infos p {
+            font-weight: lighter;
+        }
 
-</style>
+        @media only screen and (max-width: 769px) {
+            .title_info {
+                padding: 10px;
+                font-size: 18px;
+                width:90%;
+            }
+            .info {
+            display: block;
+            width: 100%;
+            margin: auto;
+            text-align: center;
+            }
+            .info1>img, .info2>img, .info3>img{
+            padding: 10px;
+            }
+        }
+
+
+
+        .plans_container {
+            display: flex;
+            width: 100%;
+        }
+
+        .promos {
+            display: flex;
+            flex-direction: column;
+            width: 50%;
+            border: 1px solid black;
+            justify-content: center;
+            align-items: center;
+        }
+
+        .promo_title {
+            text-align: center;
+            padding: 10px 0;
+        }
+        .promo_title h1 {
+            text-align: center;
+            font-weight: bolder;
+            font-size:26px;
+        }
+
+        .plans {
+            display: grid;
+            grid-template-columns: repeat(2, 1fr);
+            grid-template-rows: repeat(2, 1fr);
+            grid-column-gap: 45px;
+            grid-row-gap: 10px;
+            padding: 10px;
+        }
+
+        .plan1,
+        .plan2,
+        .plan3,
+        .plan4 {
+            padding: 20px;
+            width: 200px;
+            background-color: rgba(205, 205, 205, 0.214);
+            border: .10em solid rgba(122, 122, 122, 0.426);
+            border-radius: 20px;
+        }
+
+        .mbps {
+            font-size: 30px;
+        }
+
+        .promo_details {
+            background-image: linear-gradient(rgba(255, 255, 255, 0.5), rgba(22, 21, 25, 0.7)), url(assets/familyinternet.jpg);
+            background-position: center;
+            background-size: cover;
+            background-repeat: no-repeat;
+            position: relative;
+            width: 50%;
+            padding: 20px;
+        }
+
+        @media only screen and (max-width: 768px) {
+            .promos {
+            padding:10px
+            }
+        .plan1,
+        .plan2,
+        .plan3,
+        .plan4 {
+            padding: 10px;
+            width: 135px;
+            background-color: rgba(205, 205, 205, 0.214);
+            border: .10em solid rgba(122, 122, 122, 0.426);
+            border-radius: 20px;
+        }
+        .plans {
+            display: grid;
+            grid-template-columns: repeat(2, 1fr);
+            grid-template-rows: repeat(2, 1fr);
+            grid-column-gap: 10px;
+            grid-row-gap: 5px;
+            padding: 45px;
+        }
+        .mbps {
+            font-size: 20px;
+        }
+        .promo_title h1{
+            font-size: 21px;
+        }
+        }
+
+        /* End homepage.blade CSS style */
+    </style>
 </head>
-<body>
-@include('header')
-@yield('content')
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+<body>
+    @include('header')
+    @yield('content')
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
+    </script>
 </body>
+
 </html>
