e50907dfbd7d9b51f25a8071fa628aa1fc4b7fd2
thoamrigabito <tymagbito26@gmail.com>
2024-02-07 15:49:34 +0800

    new file:   public/assets/internet.png     new file:   public/assets/productivity.png 	new file:   public/assets/stream.png 	modified:   resources/views/homepage.blade.php 	modified:   resources/views/layout.blade.php

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -7,6 +7,87 @@
 wala na header ha? na include ko na kasi.
 no header and footer pls. Thanks.. --}}
 
+    <section class="title">
+        <h1>We’re here to help you.</h1>
+    </section>
+
+    <div class="banner">
+        <section class="chat">
+            <div class="head">
+                <h1>Chat with Us !</h1>
+                <p>Use our AJC Pisonet FB Messenger for quick and easy assistance.</p>
+                <div class="bannerbtn">
+                    <button><i class="fa-solid fa-comments btnicon"></i>UJC Chatbot</button>
+                </div>
+            </div>
+        </section>
+
+        <section class="support">
+            <div class="head">
+                <h1>Support</h1>
+                <p>We’ll find the best solution for you.</p>
+                <div class="bannerbtn">
+                    <button><i class="fa-solid fa-circle-question btnicon"></i>Go to Support</button>
+                </div>
+            </div>
+        </section>
+    </div>
+
+    <div>
+        <section class="title_info">
+            <h1>We’ve got you covered with reliable internet that’s<br/> right for your home.</h1>
+        </section>
+        <section class="info">
+            <div class="infos">
+                <img src="./assets/productivity.png">
+                <h4>Be more productive than ever</h4>
+                <p>with faster speeds that Fiber Unli can provide.</p>
+            </div>
+            <div class="infos">
+                <img src="./assets/stream.png">
+                <h4>There’s no limit!</h4>
+                <p>Stream to your heart’s content for as low as P899!</p>
+            </div>
+            <div class="infos">
+                <img src="./assets/internet.png">
+                <h4>Win more rounds with AUJ Internet</h4>
+            </div>
+        </section>
+    </div>
+
+    <div class="plans_container">
+        <section class="promos">
+            <div class="promo_title">
+                <h1>Check out our high-speed Fiber Unli Plans</h1>
+            </div>
+            <div class="plans">
+                <div class="plan1">
+                    <h3 class="mbps">200 Mbps</h3>
+                    <p>Fiber Unli Plan 1699</p>
+                </div>
+                <div class="plan2">
+                    <h3 class="mbps">400 Mbps</h3>
+                    <p>Fiber Unli Plan 1699</p>
+                </div>
+                <div class="plan3">
+                    <h3 class="mbps">600 Mbps</h3>
+                    <p>Fiber Unli Plan 1699</p>
+                </div>
+                <div class="plan4">
+                    <h3 class="mbps">1000 Mbps</h3>
+                    <p>Fiber Unli Plan 1699</p>
+                </div>
+            </div>
+        </section>
+        <section class="promo_details">
+            <div>
+                <!-- <img class="plan_img" src="./assets/familyinternet.jpg"> -->
+                <h1>200 Mbps</h1>
+            </div>
+        </section>
+    </div>
+
+
     <h1>This is Homepage</h1>
 
 
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -28,6 +28,158 @@
         color: white;
     }
 
+    .banner{
+    display: flex;
+    align-items: center;
+    width: 100%;
+    }
+
+    .title{
+        padding: 40px 0;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        background-color: rgba(162, 162, 162, 0.134);
+    }
+
+    .title h1{
+        font-size: 38px;
+        color:#c43a3c;
+        background-color: transparent;
+    }
+    .chat{
+        width: 50%;
+        background-color: #F8EFE6;
+    }
+    .head {
+        padding: 20px 0;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        background-color: transparent;
+    }
+
+    .head h1{
+        background: transparent;
+        font-size: 34px;
+        font-weight: 900;
+        padding: 50px 0 35px;
+    }
+    .head p{
+        background: transparent;
+    }
+
+    .support{
+        align-items: center;
+        width:50%;
+        background-color:#F3EED8
+    }
+
+    .bannerbtn{
+        background: transparent;
+    }
+
+    .bannerbtn button{
+        margin:18px 0 0;
+        padding: .50em .30em;
+        font-size: 16px;
+        border-radius: 25px;
+        width: 178px;
+        border: none;
+        outline: none;
+        background-color: #c43a3c;
+        color:#fff;
+        font-size: 17px;
+        text-transform: uppercase;
+        justify-items: center;
+    }
+
+    .btnicon{
+    background: transparent;
+    padding: 0 .5em 0 0;
+    }
+
+    .title_info{
+        padding-top: 50px;
+        font-size: 25px;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        text-align: center;
+    }
+
+    .info{
+        display: flex;
+        width: 100%;
+        justify-content: space-around;
+    }
+
+    .infos{
+        padding: 20px 0;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        background-color: transparent;
+    }
+    .infos img{
+        padding: 50px;
+    }
+    .infos p{
+        font-weight: lighter;
+    }
+
+
+    .plans_container{
+        display: flex;
+        width: 100%;
+    }
+    .promos{
+        display: flex;
+        flex-direction: column;
+        width: 50%;
+        border: 1px solid black;
+        justify-content: center;
+        align-items: center;
+    }
+    .promo_title{
+        padding: 10px 0;
+    }
+    .plans{
+        display: grid;
+        grid-template-columns: repeat(2, 1fr);
+        grid-template-rows: repeat(2, 1fr);
+        grid-column-gap: 45px;
+        grid-row-gap: 10px;
+        padding: 45px;
+
+    }
+
+    .plan1, .plan2, .plan3, .plan4{
+    padding: 20px;
+    width: 200px;
+    background-color: rgba(205, 205, 205, 0.214);
+    border: .10em solid rgba(122, 122, 122, 0.426);
+    border-radius: 20px;
+    }
+
+    .mbps{
+        font-size: 30px;
+    }
+
+    .promo_details{
+    background-image: linear-gradient(rgba(255, 255, 255, 0.5), rgba(22, 21, 25, 0.7)), url(assets/familyinternet.jpg);
+    background-position: center;
+    background-size: cover;
+    background-repeat: no-repeat;
+    position: relative;
+    width: 50%;
+    padding: 20px;
+    }
+
 </style>
 </head>
 <body>
